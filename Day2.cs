f597975772fbdbca47a663d074b0ca9777acf976
cavanfarrell <cavan.farrell@icloud.com>
2024-12-02 18:21:33 -0600

    day2's garbage

---
+++ Day2.cs
@@ -0,0 +1,71 @@
+namespace AdventOfCode2024;
+
+public class Day2
+{
+    private const string InputPath = "../../../inputs/day2.txt";
+
+    public static int Day2_Part1()
+    {
+        var file = Utilities.LoadFileAsLines(InputPath);
+        var reportList = ParseReport(file);
+
+        return reportList.Count(ProcessReport);
+    }
+
+    public static int Day2_Part2()
+    {
+        var file = Utilities.LoadFileAsLines(InputPath);
+        var reportList = ParseReport(file);
+
+        return reportList.Count(ProcessReportDampener);
+    }
+
+    private static List<List<int>> ParseReport(List<string> input)
+    {
+        return input.Select(l => l.Split(" ").Select(Int32.Parse).ToList()).ToList();
+    }
+
+    private static List<int> GenerateDeltas(List<int> report)
+    {
+        var deltas = new List<int>();
+
+        foreach (var (level, index) in report.Select((l, i) => (l, i)))
+        {
+            if (index < report.Count - 1)
+            {
+                deltas.Add(report[index + 1] - level);
+            }
+        }
+
+        return deltas;
+    }
+
+    private static bool ProcessReport(List<int> report)
+    {
+        var deltas = GenerateDeltas(report);
+
+        return deltas.All(n => n != 0 && Math.Abs(n) <= 3) && (deltas.All(n => n > 0) || deltas.All(n => n < 0));
+    }
+
+    private static bool ProcessReportDampener(List<int> report)
+    {
+        var processedReport = ProcessReport(report);
+
+        if (!processedReport)
+        {
+            for (var i = 0; i < report.Count; i++)
+            {
+                var tempReport = report.Select(n => n).ToList();
+                tempReport.RemoveAt(i);
+                if (ProcessReport(tempReport))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        return processedReport;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,4 +2,5 @@
 
 using AdventOfCode2024;
 
-Utilities.PrintResults(1, new List<object> {Day1.Day1_Part1(), Day1.Day1_Part2()});
+// Utilities.PrintResults(1, new List<object> {Day1.Day1_Part1(), Day1.Day1_Part2()});
+Utilities.PrintResults(2, new List<object> {Day2.Day2_Part1(), Day2.Day2_Part2()});
