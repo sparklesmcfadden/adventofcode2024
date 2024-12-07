b843cc44137e796e817b6126c534c5751835998a
cavanfarrell <cavan.farrell@icloud.com>
2024-12-07 17:43:51 -0600

    adds appsettings

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using AdventOfCode2024;
+using Microsoft.Extensions.Configuration;
 
-// Day1.Run();
-// Day2.Run();
-// Day3.Run();
-// Day4.Run();
-// Day5.Run();
-// Day6.Run();
+var builder = new ConfigurationBuilder();
+builder.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+IConfiguration config = builder.Build();
+
+var inputPath = config["input_directory"];
+
+Day1.Run();
+Day2.Run();
+Day3.Run();
+Day4.Run();
+Day5.Run();
+Day6.Run();
 Day7.Run();
