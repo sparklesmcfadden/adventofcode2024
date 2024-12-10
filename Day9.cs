ae7644219ac27b18148dd04e514d01761d620223
cavanfarrell <cavan.farrell@icloud.com>
2024-12-09 21:19:21 -0600

    day9part2

--- Day9.cs
+++ Day9.cs
@@ -1,26 +1,11 @@
 namespace AdventOfCode2024;
 
 public static class Day9
-{   
+{
     public static long Part1(string inputPath)
     {
         var input = Utilities.LoadFile(inputPath);
-
-        var fileId = 0;
-        var blockList = new List<int>();
-        for (var i = 0; i < input.Length; i++)
-        {
-            var counter = Int32.Parse(input[i].ToString());
-            if (i % 2 == 0)
-            {
-                blockList.AddRange(Enumerable.Repeat(fileId, counter));
-                fileId++;
-            }
-            if (i % 2 != 0)
-            {
-                blockList.AddRange(Enumerable.Repeat(-1, counter));
-            }
-        }
+        var blockList = MapToBlocks(input);
 
         var lastBlockIndex = blockList.Count - 1;
         for (var i = 0; i < blockList.Count; i++)
@@ -43,16 +28,131 @@ public static class Day9
 
         return CalculateChecksum(blockList);
     }
-    
+
     public static long Part2(string inputPath)
     {
-        var input = Utilities.LoadFileAsLines(inputPath);
-        
-        return 0;
+        var input = Utilities.LoadFile(inputPath);
+        var blockList = MapToBlockFiles(input);
+
+        for (var i = blockList.Count - 1; i >= 0; i--)
+        {
+            var fileToMove = blockList[i];
+            for (var j = 0; j < i; j++)
+            {
+                if (blockList[j].FileId == -1) // it is free space
+                {
+                    var blankLength = blockList[j].Length;
+                    if (blankLength >= fileToMove.Length)
+                    {
+                        blockList[j] = fileToMove;
+                        blockList[i] = new BlockFile(-1, fileToMove.Length);
+                        if (blankLength > fileToMove.Length)
+                        {
+                            blockList.Insert(j + 1, new BlockFile(-1, blankLength - fileToMove.Length));
+                            blockList = MergeFreeSpace(blockList);
+                        }
+
+                        break;
+                    }
+                }
+            }
+        }
+
+        var blocks = BlockFilesToBlocks(blockList);
+        return CalculateChecksum2(blocks);
+    }
+
+    private static List<BlockFile> MergeFreeSpace(List<BlockFile> blockFiles)
+    {
+        for (var i = blockFiles.Count - 1; i > 1; i--)
+        {
+            if (blockFiles[i].FileId == -1 && blockFiles[i - 1].FileId == -1)
+            {
+                blockFiles[i].Length += blockFiles[i - 1].Length;
+                blockFiles.RemoveAt(i - 1);
+            }
+        }
+
+        return blockFiles;
+    }
+
+    private static List<BlockFile> MapToBlockFiles(string diskMap)
+    {
+        var fileId = 0;
+        var blockList = new List<BlockFile>();
+        for (var i = 0; i < diskMap.Length; i++)
+        {
+            var counter = Int32.Parse(diskMap[i].ToString());
+            if (i % 2 == 0)
+            {
+                blockList.Add(new BlockFile(fileId, counter));
+                fileId++;
+            }
+
+            if (i % 2 != 0)
+            {
+                blockList.Add(new BlockFile(-1, counter));
+            }
+        }
+
+        return blockList;
+    }
+
+    private static List<int> BlockFilesToBlocks(List<BlockFile> blockFiles)
+    {
+        var blockList = new List<int>();
+        foreach (var blockFile in blockFiles)
+        {
+            blockList.AddRange(Enumerable.Repeat(blockFile.FileId, blockFile.Length));
+        }
+
+        return blockList;
+    }
+
+    private static List<int> MapToBlocks(string diskMap)
+    {
+        var fileId = 0;
+        var blockList = new List<int>();
+        for (var i = 0; i < diskMap.Length; i++)
+        {
+            var counter = Int32.Parse(diskMap[i].ToString());
+            if (i % 2 == 0)
+            {
+                blockList.AddRange(Enumerable.Repeat(fileId, counter));
+                fileId++;
+            }
+
+            if (i % 2 != 0)
+            {
+                blockList.AddRange(Enumerable.Repeat(-1, counter));
+            }
+        }
+
+        return blockList;
     }
 
     private static long CalculateChecksum(List<int> fileBlocks)
     {
         return fileBlocks.TakeWhile(t => t >= 0).Select(Convert.ToInt64).Select((t, i) => i * t).Sum();
+    }
+
+    private static long CalculateChecksum2(List<int> fileBlocks)
+    {
+        long checksum = 0;
+        for (var i = 0; i < fileBlocks.Count; i++)
+        {
+            if (fileBlocks[i] > 0)
+            {
+                checksum += fileBlocks[i] * i;
+            }
+        }
+
+        return checksum;
+    }
+
+    private class BlockFile(int fileId, int length)
+    {
+        public int FileId { get; set; } = fileId;
+        public int Length { get; set; } = length;
     }
 }
